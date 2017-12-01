fb147f5fd97a03c6e795bac72aa73ff5840d8d48
EvgenBabenko <__>
2017-12-01 12:00:04 +0200

    Complete main logic functional

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,38 +1,52 @@
 // jshint esversion: 6
 
-let grid = document.querySelector('.grid');
+const grid = document.querySelector('.grid');
 
-let pairClick = [];
-
-let click;
-
-let count = 0;
-let countCard = 0;
-
-let gridSize = document.getElementById('selectSize').value;
-console.log('gridSize', gridSize);
-
+let pairOfCard;
+let countClick;
+let countFindedPair;
+let gridSize;
 
 const appPath = '../img/';
-// const arr = ['animals-bunny.jpg', 'animals-bunny-2.jpg', 'animals-cat.jpg', 'animals-cat-2.jpg', 'animals-dog.jpg', 'animals-dog-2.jpg', 'animals-horse.jpg', 'animals-horse-2.jpg', 'architecture-london-towerbridge.jpg'];
-const arr = [];
+const arr = ['animals-bunny.jpg', 'animals-bunny-2.jpg', 'animals-cat.jpg', 'animals-cat-2.jpg', 'animals-dog.jpg', 'animals-dog-2.jpg', 'animals-horse.jpg', 'animals-horse-2.jpg', 'architecture-london-towerbridge.jpg'];
 
-function endGame() {
-  if (countCard === +gridSize*2) {
-    alert('Congratulations!');
-  }
+
+//-------------------------------------------
+//---------------POINT OF ENTRY
+//-------------------------------------------
+
+
+function init(gridSize = 2) {
+  // debugger
+  console.log('gridSize', gridSize);
+  pairOfCard = [];
+  countClick = 0;
+  countFindedPair = 0;
+
+  totalCards = parseInt(gridSize * gridSize, 10);
+  totalPairs = totalCards / 2;
+
+  message('Find all the pairs!');
+  createGrid();
+  renderGrid(gridSize);
+
+  grid.addEventListener('click', clickCard);
 }
+
+init();
+
+
+//-------------------------------------------
+//---------------MAIN LOGIC
+//-------------------------------------------
 
 
 function createGrid() {
-  let newArray = shuffle(arr);
+  // debugger
+  let newArray = createArray();
   console.log('newArray', newArray);
 
-
-
-  let numberCard = gridSize * gridSize / 2;
-
-  for (let i = 0; i < numberCard * 2; i++) {
+  for (let i = 0; i < totalCards; i++) {
     console.log(i);
     let div = document.createElement('div');
     div.className = 'mem-card';
@@ -42,17 +56,13 @@ function createGrid() {
     div.appendChild(img);
     img.src = appPath + newArray[i];
   }
-
-  renderGrid();
 }
 
-createGrid();
 
-function renderGrid() {
+function renderGrid(gridSize) {
   const gridWidth = document.querySelector('.grid').offsetWidth;
 
   let cardSize = gridWidth / gridSize;
-
 
   let card = [...document.querySelectorAll('.mem-card')];
   card.forEach(elem => {
@@ -62,16 +72,99 @@ function renderGrid() {
 }
 
 
+function clickCard(e) {
+  if (!e.target.classList.contains('mem-card')) {
+    return;
+  }
 
-function changeSize() {
-  createGrid();
+  show(e.target.firstElementChild);
 
+  countClick++;
+  console.log('number of click', countClick);
+  pairOfCard.push(e.target.firstElementChild);
+  checkClick();
 }
 
 
+function checkClick() {
+  if (pairOfCard.length == 2 && pairOfCard[0].src === pairOfCard[1].src) {
+    hideCards();
+    countFindedPair++;
+    endGame();
+  } else if (pairOfCard.length == 2) {
+    closeCards();
+  }
+
+  message(`You found ${countFindedPair} out of ${totalPairs} pairs with ${countClick / 2} tries.`);
+}
+
+
+function message(text) {
+  const messageText = document.querySelector('.message');
+  messageText.innerHTML = text;
+}
+
+
+function hideCards() {
+  setTimeout(() => {
+    pairOfCard.forEach(elem => {
+      elem.parentNode.classList.add('empty');
+      hide(elem);
+    });
+    pairOfCard = [];
+  }, 500);
+}
+
+
+function closeCards() {
+  setTimeout(() => {
+    pairOfCard.forEach(elem => {
+      hide(elem);
+    });
+    pairOfCard = [];
+  }, 500);
+}
+
+
+function show(node) {
+  node.style.display = 'block';
+}
+
+
+function hide(node) {
+  node.style.display = 'none';
+}
+
+
+function endGame() {
+  if (countFindedPair === totalPairs) {
+    alert('Congratulations, you found them all!');
+  }
+}
+
+
+function reset() {
+  while (grid.firstElementChild) {
+    grid.firstElementChild.remove();
+  }
+
+  init();
+}
+
+
+function createArray() {
+  let newArr = [];
+  for (let i = 1; i <= totalPairs; i++) {
+    newArr.push(arr[i]);
+  }
+  console.log(newArr);
+  let doubleArr = [...shuffle(newArr), ...shuffle(newArr)];
+  console.log(doubleArr);
+  return doubleArr;
+}
+
 
 function shuffle(array) {
-
   let result = [...array];
 
   result.sort((a, b) => {
@@ -79,68 +172,4 @@ function shuffle(array) {
   });
 
   return result;
-}
-
-
-
-function clickCard(event) {
-  if (!event.target.classList.contains('mem-card')) return;
-
-  show(event.target.firstElementChild);
-
-  console.log('number of click', count++);
-  pairClick.push(event.target.firstElementChild);
-  click = event.target.firstElementChild.src;
-}
-
-grid.addEventListener('click', clickCard);
-grid.addEventListener('click', checkClick);
-
-
-
-function checkClick() {
-
-  if (pairClick.length == 2 && pairClick[0].src === pairClick[1].src) {
-    setTimeout(() => {
-      pairClick.forEach(elem => {
-        elem.parentNode.classList.add('empty');
-        hide(elem);
-      });
-      pairClick.length = 0;
-    }, 500);
-    countCard++;
-    console.log('countCard', countCard);
-    endGame();
-  } else if (pairClick.length == 2) {
-    setTimeout(() => {
-      pairClick.forEach(elem => {
-        hide(elem);
-      });
-      pairClick.length = 0;
-    }, 500);
-  }
-}
-
-
-
-function show(node) {
-  node.style.display = 'block';
-}
-
-function hide(node) {
-  node.style.display = 'none';
-}
-
-
-function restart() {
-  const listImages = document.querySelectorAll('.mem-img');
-  const arrImages = [...listImages];
-  console.log(arrImages);
-
-  pairClick.length = 0;
-
-  arrImages.forEach(elem => {
-    hide(elem);
-    elem.parentNode.classList.remove('empty');
-  });
 }
